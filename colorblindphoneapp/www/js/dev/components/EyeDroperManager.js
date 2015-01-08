79ac612504bc43c74d68dd4e2edbd996b076c862
Bryson <brysonwf@gmail.com>
2015-01-07 22:10:46 -0500

    added in jquery eyedropper

--- colorblindphoneapp/www/js/dev/components/EyeDroperManager.js
+++ colorblindphoneapp/www/js/dev/components/EyeDroperManager.js
@@ -44,22 +44,12 @@ EyeDroperManager = {
             var reader = new FileReader();
             reader.onload = (function (aImg, aCtx, sCtx) {
             return function (e) {
-            aImg.onload = onload;
-            aImg.src = e.target.result;
+                aImg.onload = onload;
+                aImg.src = e.target.result;
             };
-        })(img, ctx, sctx);
-        reader.readAsDataURL(file);
+            })(img, ctx, sctx);
+            reader.readAsDataURL(file);
         }
         }
-
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-6804458-3']);
-        _gaq.push(['_trackPageview']);
-
-        (function () {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
     }
 }
