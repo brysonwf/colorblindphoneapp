e0c6788e590ffc2d6f74595e7c8cedcf283a3580
Bryson <brysonwf@gmail.com>
2015-01-14 18:48:50 -0500

    hooking up colorpicker and camera function

--- colorblindphoneapp/www/js/dev/components/EyeDroperManager.js
+++ colorblindphoneapp/www/js/dev/components/EyeDroperManager.js
@@ -3,8 +3,6 @@ var EyeDroperManager = {
 
         $('img').on('click', function(e){
 
-            alert('this');
-
             if(!this.canvas) {
                 this.canvas = $('<canvas />')[0];
                 this.canvas.width = this.width;
@@ -14,7 +12,9 @@ var EyeDroperManager = {
 
             var pixelData = this.canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
 
-            $('#output').html('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
+            alert('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
+
+//            $('#output').html('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
 
         });
 
