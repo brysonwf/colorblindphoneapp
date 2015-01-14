0603c8adc163108589e013d7eee0ce298f44c272
Bryson <brysonwf@gmail.com>
2015-01-14 18:51:46 -0500

    hooking up colorpicker and camera function

--- colorblindphoneapp/www/js/live/base.js
+++ colorblindphoneapp/www/js/live/base.js
@@ -109,8 +109,6 @@ var CameraManager = {
 
         $('img').on('click', function(e){
 
-            alert('this');
-
             if(!this.canvas) {
                 this.canvas = $('<canvas />')[0];
                 this.canvas.width = this.width;
@@ -120,10 +118,11 @@ var CameraManager = {
 
             var pixelData = this.canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
 
-            $('#output').html('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
+            alert('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
+
+//            $('#output').html('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
 
         });
-
 
     }
 };jQuery(document).ready(function(){
